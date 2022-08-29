d114f9e5e5cf0100a64b14e80549056f50ca5a27
Juliana Ferreira <julianafercavalcante@hotmail.com>
2022-08-29 16:56:46 -0300

    gabaritos

--- Aulas/aula 07 (strings)/string-list-generator/gabarito.js
+++ Aulas/aula 07 (strings)/string-list-generator/gabarito.js
@@ -1,3 +1,14 @@
-function createList(length, text='Text') {
-    let list = '<ul>\n'
-}
+function createList(length, text = 'Text') {
+    let list = '<ul>\n';
+  
+    for (let x = 1; x < length+1; x++) {
+      list += `  <li>${text} ${x}</li>\n`;
+    }
+  
+    list += '</ul>';
+  
+    return list;
+  }
+  
+  console.log(createList(3));
+  console.log(createList(5, 'Item'));
